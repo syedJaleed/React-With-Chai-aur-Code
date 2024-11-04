8ec07bd12960b777be6243732b02615e51251572
Jaleed <syedjaleedah29@gmail.com>
2024-11-04 21:33:39 +0530

    Created Projects with Vite

---
+++ chai_react/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+   <h1>Chai Aur react</h1>
+  );
+}
+
+export default App;
---
+++ viteproject/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <h1>Chai with Vite | Jaleed</h1>
+  )
+}
+
+export default App
