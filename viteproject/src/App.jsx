3ba4b99a5899c093fab994ae3312c60e999e343d
Jaleed <syedjaleedah29@gmail.com>
2024-11-15 14:38:27 +0530

    New react in JS

--- viteproject/src/App.jsx
+++ viteproject/src/App.jsx
@@ -3,8 +3,13 @@ import Chai from './chai'
 
 function App() {
 
+  const username = 'jaleed';
+
   return (
+    <>
    <Chai/>
+   <p>This is para {username}</p>
+   </>
   )
 }
 
--- viteproject/src/main.jsx
+++ viteproject/src/main.jsx
@@ -1,8 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-    <App />
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom App</h1>
+        </div>
+    )
+}
+
+const anotherElement = (
+    <a href='https://google.com'>visit</a>
+)
+
+const anotherUser = "Syed"
+
+const reactElement = React.createElement(
+    "a",
+    {href: 'https://google.com', target: '_blank'},
+    'Click me',
+    anotherUser
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App/>
 )
