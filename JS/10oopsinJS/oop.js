8a354e604b7ac19c18f12f0b46074b9678fb3628
Jaleed <syedjaleedah29@gmail.com>
2024-12-19 21:32:30 +0530

    Opps

---
+++ JS/10oopsinJS/oop.js
@@ -0,0 +1,10 @@
+const user = {
+    username: 'Jaleed',
+    loginCount: 8,
+    signedIn: false,
+    getUserDetails: function(){
+        console.log("Wow");
+    }
+}
+
+console.log(user.getUserDetails());
